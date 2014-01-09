f8bab87769e1fe0fee17f25e8da5e75324c3a2e7
Shad Campbell <shadc@gmx.com>
2014-01-09 14:18:06 -0800

    Remove dgrid and update Esri JSAPI version to 3.8.  Version 3.7 of the Esri JSAPI included a version of dgrid that did not support toggling of items in a list by clicking them so a newer version of dgrid was added to override the Esri version.  3.8 now includes the latest version of dgrid so the added version was removed.

--- js/dojoConfig.js
+++ js/dojoConfig.js
@@ -3,6 +3,5 @@ var dojoConfig = {
     extras: location.pathname.replace(/\/[^/]+$/, "") + "/js/modules",
     lists: location.pathname.replace(/\/[^/]+$/, "") + "/js/lists",
     templates: location.pathname.replace(/\/[^/]+$/, "") + "/js/templates",
-    dgrid: location.pathname.replace(/\/[^/]+$/, "") + "/js/dgrid"
   }
 };
